948d6f90deb9b4decf8ab15062335d571669e104
speier <kalman.speier@gmail.com>
2020-01-16 10:51:31 +0100

    gdpr docs pdf

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,7 @@
 module.exports = (eleventyConfig) => {
   eleventyConfig.addPassthroughCopy("banners")
   eleventyConfig.addPassthroughCopy("css")
+  eleventyConfig.addPassthroughCopy("docs")
   eleventyConfig.addPassthroughCopy("gallery")
   eleventyConfig.addPassthroughCopy("images")
   eleventyConfig.addPassthroughCopy("js")
