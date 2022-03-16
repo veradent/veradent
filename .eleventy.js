8e6b849b67bd1f1452d297d3ceb4e6e0533a2690
speier <speier@users.noreply.github.com>
2022-03-16 15:55:57 +0000

    deploy: b7da34bff910dcabe47946224e9a00e019fcd76d

--- .eleventy.js
+++
@@ -1,8 +0,0 @@
-module.exports = (eleventyConfig) => {
-  eleventyConfig.addPassthroughCopy("banners")
-  eleventyConfig.addPassthroughCopy("css")
-  eleventyConfig.addPassthroughCopy("docs")
-  eleventyConfig.addPassthroughCopy("gallery")
-  eleventyConfig.addPassthroughCopy("images")
-  eleventyConfig.addPassthroughCopy("js")
-}
